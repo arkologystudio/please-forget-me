3112e87529690799cffec68f0a56c1f6bcc6a919
Ross Eyre <rosseyre@gmail.com>
2024-12-12 17:46:05 +0200

    Add APP router route

---
+++ prisma/seed.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@prisma/client'
+import { organisations } from "./organisations";
+
+const prisma = new PrismaClient();
+
+export type OrganisationDB = {
+  name: string;
+  email: string;
+  slug: string;
+};
+
+async function main() {
+  const createdOrgs = [];
+
+  for (const org of organisations) {
+    const existingOrg = await prisma.organisation.findUnique({
+      where: { slug: org.slug },
+    });
+
+    if (existingOrg) {
+      console.log(`Organisation ${org.name} already exists:`, existingOrg);
+      continue;
+    }
+
+    const createdOrg = await prisma.organisation.create({
+      data: org as OrganisationDB,
+    });
+    createdOrgs.push(createdOrg);
+  }
+
+  console.log("Created organisations:", createdOrgs);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
